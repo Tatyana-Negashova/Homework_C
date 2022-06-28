500601e43f709d55483fa30ee3664dd8b2c2c2e4
tanchik <negashovata@gmail.com>
2022-06-28 21:52:52 +0300

    add zadacha4

---
+++ Zadacha4/Program.cs
@@ -0,0 +1,10 @@
+﻿int a,b,c;
+int max = 0;
+Console.WriteLine("Введите три числа ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+max = (Math.Max(a,Math.Max(b, c)));
+Console.WriteLine("{0} - максимальное число", max);
+
+
