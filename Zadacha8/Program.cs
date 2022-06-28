623faba1e1175121f649b042c101d98c1f5129cb
tanchik <negashovata@gmail.com>
2022-06-28 23:16:27 +0300

    add zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a < N);
+{
+if  (a % 2 == 0) 
+{
+
+Console.Write(a + ",");
+}
+a += 1;
+}
