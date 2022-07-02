52e3df70cd304088353a7558c449450eee015369
tanchik <negashovata@gmail.com>
2022-07-02 08:17:00 +0300

    решаем проблемы

--- Zadacha2/Program.cs
+++ Zadacha2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿int a,b;
+Console.WriteLine("Введите два числа ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{Console.WriteLine("{0} наибольшее число", a)}
+else
+{ Console.WriteLine("{0} наибольшее число", b)}
+
