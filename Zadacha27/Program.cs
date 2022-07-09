66b4ba68334fc95ae7d83a26b460ea2f1b8f4045
tanchik <negashovata@gmail.com>
2022-07-09 19:14:14 +0300

    добавлена 27 задача

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,18 @@
+﻿﻿// Напишите функцию, которая принимает на вход число и выдает
+// сумму цифр в числе.
+
+System.Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine()!);
+
+void Summa(int a)
+{
+    int temp = 0;
+    for (; a > 0; a /= 10)
+    {
+        temp += a % 10;
+    }
+    System.Console.WriteLine(temp);
+}
+
+Summa(Number);
+
