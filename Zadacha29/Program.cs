5e84abc5e1d1ecafddaba9d31649c989eab509d5
tanchik <negashovata@gmail.com>
2022-07-09 19:30:22 +0300

    добавлена 29 задача

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задает массив из некоторого количества
+// элементов и выводит их на экран с помощью функций
+
+
+int[] Collection(int elements)
+{
+    int[] collection = new int[elements];
+    Console.WriteLine("Заполните массив: ");
+
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.WriteLine();
+        Console.Write($"Введите элемент массива № {i}: ");
+        collection[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    Console.WriteLine("Числа массива: ");
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write($"{collection[i]} ");
+
+    }
+    return collection;
+}
+Console.Write("Введите длину массива: ");
+int SizeArray = Convert.ToInt32(Console.ReadLine());
+
+
+Collection(SizeArray);
+
+
